f8707c915ab4d23339e1c90958b90bd8688beb64
flo <flo@flos-MacBook-Pro.local>
2022-12-13 14:17:35 +0100

    Breaking change. Add config parameter

--- readlines.go
+++ readlines.go
@@ -3,16 +3,42 @@ package readlines
 import (
 	"bufio"
 	"io"
+	"os"
 )
 
-func ReadLines(reader io.Reader) <-chan string {
+type Config struct {
+	Number          uint
+	ApplyOnEachLine func(s string) string
+}
+
+func ReadLinesFromPath(path string, config *Config) <-chan string {
+	fd, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	return ReadLines(fd, config)
+}
+
+func ReadLines(reader io.Reader, config *Config) <-chan string {
 	ret := make(chan string)
+	var count uint = 0
 	go func() {
+		defer close(ret)
 		scanner := bufio.NewScanner(reader)
 		for scanner.Scan() {
-			ret <- scanner.Text()
+			line := scanner.Text()
+			if config != nil && config.ApplyOnEachLine != nil {
+				ret <- config.ApplyOnEachLine(line)
+			} else {
+				ret <- line
+			}
+			count++
+			if config != nil && config.Number > 0 {
+				if count >= config.Number {
+					return
+				}
+			}
 		}
-		close(ret)
 	}()
 	return ret
 }
--- readlines_test.go
+++ readlines_test.go
@@ -2,7 +2,10 @@ package readlines
 
 import (
 	"bytes"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReadLines(t *testing.T) {
@@ -12,8 +15,67 @@ two
 three`
 	buff := bytes.NewBufferString(text)
 	count := 0
-	for range ReadLines(buff) {
+	for range ReadLines(buff, nil) {
 		count++
+	}
+	assert.Equal(t, count, 3)
+}
+func TestReadLinesNumber(t *testing.T) {
+	text :=
+		`one
+two
+three`
+	buff := bytes.NewBufferString(text)
+	count := 0
+	for range ReadLines(buff, &Config{Number: 1}) {
+		count++
+	}
+	assert.Equal(t, count, 1)
+}
+
+func TestReadLinesGreaterNumber(t *testing.T) {
+	text :=
+		`one
+two
+three`
+	buff := bytes.NewBufferString(text)
+	count := 0
+	for range ReadLines(buff, &Config{Number: 14}) {
+		count++
+	}
+	assert.Equal(t, count, 3)
+}
+
+func TestReadLinesZeroNumber(t *testing.T) {
+	text :=
+		`one
+two
+three`
+	buff := bytes.NewBufferString(text)
+	count := 0
+	for range ReadLines(buff, &Config{Number: 0}) {
+		count++
+	}
+	assert.Equal(t, count, 3)
+}
+func TestCapitalize(t *testing.T) {
+	text :=
+		`one
+two
+three`
+	buff := bytes.NewBufferString(text)
+	count := 0
+	for line := range ReadLines(buff, &Config{ApplyOnEachLine: strings.ToUpper}) {
+
+		count++
+		switch count {
+		case 1:
+			assert.Equal(t, line, "ONE")
+		case 2:
+			assert.Equal(t, line, "TWO")
+		case 3:
+			assert.Equal(t, line, "THREE")
+		}
 	}
 	if count != 3 {
 		t.Fatalf("expected 3 lines got %v", count)
